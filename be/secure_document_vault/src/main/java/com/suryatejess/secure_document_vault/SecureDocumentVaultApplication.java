b01b08e7d818116e282390e56fb70dd0a5400599
Surya Tejess <surya.tejesskarri@vedantu.com>
2026-09-03 22:24:20 +0530

    initial commit

---
+++ be/secure_document_vault/src/main/java/com/suryatejess/secure_document_vault/SecureDocumentVaultApplication.java
@@ -0,0 +1,13 @@
+package com.suryatejess.secure_document_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureDocumentVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureDocumentVaultApplication.class, args);
+    }
+
+}
---
+++ be/secure_document_vault/src/test/java/com/suryatejess/secure_document_vault/SecureDocumentVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suryatejess.secure_document_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDocumentVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
